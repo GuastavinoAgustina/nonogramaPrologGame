f86eb41a5b719636fe3969f239029c6500c053ef
Catalina Dumrauf <catadumrauf00@gmail.com>
2024-04-29 00:15:25 -0300

    Los cuadrados se pintan

--- src/Square.js
+++ src/Square.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-
 function Square({ value, onClick }) {
-    return (
-        <div
-            className={`square ${value === '#' ? 'relleno_negro' : 'relleno_blanco'}`}
-            onClick={onClick}
-        >
-            {value}
-        </div>
-    );
+  let className = "square";
+  if (value === '_') {
+    className += " relleno_blanco"; // Si el valor es '_', el cuadrado tiene relleno blanco
+  } else if (value === '#') {
+    className += " relleno_negro"; // Si el valor es '#', el cuadrado tiene relleno negro
+  }
+
+  return (
+    <button className={className} onClick={onClick}>
+      {value !== '_' && value} {/* Si el valor no es '_', muestra el valor */}
+    </button>
+  );
 }
 
 export default Square;
